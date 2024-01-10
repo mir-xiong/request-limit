4e0ca622b8f9f8008c9332420c42e331fbc3be2c
xmhans <xmhans@sky-cloud.net>
2024-01-10 20:13:28 +0800

    feat: 初始化项目

---
+++ src/main/java/org/eu/requestlimit/RequestLimitApplication.java
@@ -0,0 +1,13 @@
+package org.eu.requestlimit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestLimitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestLimitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/eu/requestlimit/RequestLimitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eu.requestlimit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestLimitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
